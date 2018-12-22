eaecfaab59bccc97e05eb593a09697131de86474
Artyom Pavlovich <43780397+ArtyomPavlovich@users.noreply.github.com>
2018-12-22 08:53:03 +0300

    removed chaos +img

--- js/GoogleMap.js
+++ js/GoogleMap.js
@@ -4,11 +4,11 @@ function initMap() {
         center: {lat: 53.682758, lng:  23.829920},
         zoom: 18,
     });
-    image = 'img/imgMap.png',
-        marker = new google.maps.Marker ({
-            position: {lat: 53.682758, lng:  23.829920},
-            map: map,
-            icon: image,
-            animation: google.maps.Animation.BOUNCE
-        });
+    image = 'img/imgMap.png';
+    marker = new google.maps.Marker ({
+       position: {lat: 53.682758, lng:  23.829920},
+       map: map,
+       icon: image,
+       animation: google.maps.Animation.BOUNCE
+    });
 }
